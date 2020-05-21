77fc0c52f3f7dd67b9fa0bf7e5df122fa4d730a3
Verkhoturkin <verhoturkin@gmail.com>
2020-05-21 17:04:35 +0500

    step 85

---
+++ src/com/example/app/StudentDbUtil.java
@@ -0,0 +1,39 @@
+package com.example.app;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDbUtil {
+
+    private DataSource dataSource;
+
+    public StudentDbUtil(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        String sql = "SELECT * FROM students ORDER BY last_name";
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String email = resultSet.getString("email");
+
+                students.add(new Student(id, firstName, lastName, email));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
+}
